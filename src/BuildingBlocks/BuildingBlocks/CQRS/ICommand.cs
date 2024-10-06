a422f2bc1476430d6b0e1996d83822258cbf1013
martix05 <mike.ortiz@axxis-systems.com>
2024-10-06 15:31:09 -0600

    first steps cqrs

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+/// <summary>
+/// Esta interfaz es para operaciones sin respuestas
+/// </summary>
+public interface ICommand: ICommand<Unit>
+{
+
+}
+
+
+/// <summary>
+/// Esta interfaz es para operaciones con respuestas
+/// </summary>
+/// <typeparam name="TResponse"></typeparam>
+public interface ICommand<out TResponse>: IRequest<TResponse>
+{
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,9 @@
+﻿
+using MediatR;
+
+namespace BuildingBlocks.CQRS;
+// Esta interfaz es para operaciones de lectura en el CQRS
+public interface IQuery<out TResponse>: IRequest<TResponse>
+    where TResponse: notnull
+{
+}
